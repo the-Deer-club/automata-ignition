1c3773d675459155d59d9b13b4c8cd592a5de980
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2024-08-02 03:47:52 +0700

    Init installer with linux

---
+++ debian/lang/lang.sh
@@ -0,0 +1,11 @@
+install_node () {
+    echo "Begin to install node"
+    source ./node/node.sh
+    echo "Finish Install Node"
+}
+
+install_rust () {
+    echo "Begin to install rust"
+    source ./rust/rust.sh
+    echo "Finish install rust"
+}
